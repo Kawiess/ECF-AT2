5c807cf544134af48f99451745200a2128594138
Guillaume-BL <kawiess@gmail.com>
2025-05-21 15:38:13 +1100

    Environnement de travail

---
+++ config/config.php
@@ -0,0 +1,38 @@
+<?php
+
+use Dotenv\Dotenv;
+
+// Environnement de travail.
+define('APP_ENV', getenv('APP_ENV') ?: 'development');
+
+// Si "dév", on charge le fichier ".env" local.
+if (APP_ENV === 'development') {
+    if (file_exists(__DIR__ . '/../.env')) {
+        require_once __DIR__ . '/../vendor/autoload.php';
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+    }
+}
+
+// Variables de BDD.
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_NAME', getenv('DB_NAME') ?: 'pim_db');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: 'sgbd');
+
+// Autres variables.
+define('BASE_URL', getenv('BASE_URL') ?: '/ecf-at2/public/');
+define('SESSION_NAME', getenv('SESSION_NAME') ?: 'pim_admin_session');
+define('DEBUG_MODE', getenv('DEBUG_MODE') === '1' ? true : false);
+
+// Fuseau horaire.
+date_default_timezone_set('Pacific/Noumea');
+
+// Gestion des erreurs.
+if (DEBUG_MODE) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(0);
+}
--- public/index.php
+++ public/index.php
@@ -2,26 +2,7 @@
 
 // Configuration.
 require_once __DIR__ . '/../vendor/autoload.php';
-use Dotenv\Dotenv;
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASS', $_ENV['DB_PASS']);
-define('BASE_URL', $_ENV['BASE_URL']);
-define('SESSION_NAME', $_ENV['SESSION_NAME']);
-define('DEBUG_MODE', filter_var($_ENV['DEBUG_MODE'], FILTER_VALIDATE_BOOLEAN));
-date_default_timezone_set($_ENV['TIMEZONE']);
-
-// Gestion des erreurs.
-if (DEBUG_MODE) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', 0);
-    error_reporting(0);
-}
+require_once __DIR__ . '/../config/config.php';
 
 // Démarrage de la session.
 session_name(SESSION_NAME);
