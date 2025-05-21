beeec5d610ae38ce4d6b5420360b3f0335ba39db
Guillaume-BL <kawiess@gmail.com>
2025-05-21 15:09:56 +1100

    Majuscule 2/2

--- App2/Views/dashboard/_index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>Tableau de bord</title></head>
-<body>
-    <h1>Bienvenue <?= htmlspecialchars($user['username']) ?></h1>
-    <p>Connexion réussie !</p>
-</body>
-</html>
